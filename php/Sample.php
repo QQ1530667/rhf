c02dcc182542c5f8edb8810e50df8d10d9369275
wkb1314 <1312210235@qq.com>
2016-06-17 08:21:14 +0800

    1

--- php/Sample.php
+++ php/Sample.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "./WXBizMsgCrypt.php";
+include_once "WXBizMsgCrypt.php";
 
 // 假设企业号在公众平台上设置的参数如下
 $encodingAesKey = "jWmYm7qr5nMoAUwZRjGtBxmz3KA1tkAj3ykkR6q2B2C";
@@ -41,6 +41,7 @@ if ($errCode == 0) {
 	//
 	// 验证URL成功，将sEchoStr返回
 	// HttpUtils.SetResponce($sEchoStr);
+	echo $sEchoStr;
 } else {
 	print("ERR: " . $errCode . "\n\n");
 }
@@ -65,63 +66,63 @@ Content-Length: 613
 */
 
 // $sReqMsgSig = HttpUtils.ParseUrl("msg_signature");
-$sReqMsgSig = "477715d11cdb4164915debcba66cb864d751f3e6";
-// $sReqTimeStamp = HttpUtils.ParseUrl("timestamp");
-$sReqTimeStamp = "1409659813";
-// $sReqNonce = HttpUtils.ParseUrl("nonce");
-$sReqNonce = "1372623149";
-// post请求的密文数据
-// $sReqData = HttpUtils.PostData();
-$sReqData = "<xml><ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName><Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt><AgentID><![CDATA[218]]></AgentID></xml>";
-$sMsg = "";  // 解析之后的明文
-$errCode = $wxcpt->DecryptMsg($sReqMsgSig, $sReqTimeStamp, $sReqNonce, $sReqData, $sMsg);
-if ($errCode == 0) {
-	// 解密成功，sMsg即为xml格式的明文
-	// TODO: 对明文的处理
-	// For example:
-	$xml = new DOMDocument();
-	$xml->loadXML($sMsg);
-	$content = $xml->getElementsByTagName('Content')->item(0)->nodeValue;
-	print("content: " . $content . "\n\n");
-	// ...
-	// ...
-} else {
-	print("ERR: " . $errCode . "\n\n");
-	//exit(-1);
-}
+// $sReqMsgSig = "477715d11cdb4164915debcba66cb864d751f3e6";
+// // $sReqTimeStamp = HttpUtils.ParseUrl("timestamp");
+// $sReqTimeStamp = "1409659813";
+// // $sReqNonce = HttpUtils.ParseUrl("nonce");
+// $sReqNonce = "1372623149";
+// // post请求的密文数据
+// // $sReqData = HttpUtils.PostData();
+// $sReqData = "<xml><ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName><Encrypt><![CDATA[RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==]]></Encrypt><AgentID><![CDATA[218]]></AgentID></xml>";
+// $sMsg = "";  // 解析之后的明文
+// $errCode = $wxcpt->DecryptMsg($sReqMsgSig, $sReqTimeStamp, $sReqNonce, $sReqData, $sMsg);
+// if ($errCode == 0) {
+// 	// 解密成功，sMsg即为xml格式的明文
+// 	// TODO: 对明文的处理
+// 	// For example:
+// 	$xml = new DOMDocument();
+// 	$xml->loadXML($sMsg);
+// 	$content = $xml->getElementsByTagName('Content')->item(0)->nodeValue;
+// 	print("content: " . $content . "\n\n");
+// 	// ...
+// 	// ...
+// } else {
+// 	print("ERR: " . $errCode . "\n\n");
+// 	//exit(-1);
+// }
 
-/*
-------------使用示例三：企业回复用户消息的加密---------------
-企业被动回复用户的消息也需要进行加密，并且拼接成密文格式的xml串。
-假设企业需要回复用户的明文如下：
-<xml>
-<ToUserName><![CDATA[mycreate]]></ToUserName>
-<FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName>
-<CreateTime>1348831860</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[this is a test]]></Content>
-<MsgId>1234567890123456</MsgId>
-<AgentID>128</AgentID>
-</xml>
+// /*
+// ------------使用示例三：企业回复用户消息的加密---------------
+// 企业被动回复用户的消息也需要进行加密，并且拼接成密文格式的xml串。
+// 假设企业需要回复用户的明文如下：
+// <xml>
+// <ToUserName><![CDATA[mycreate]]></ToUserName>
+// <FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName>
+// <CreateTime>1348831860</CreateTime>
+// <MsgType><![CDATA[text]]></MsgType>
+// <Content><![CDATA[this is a test]]></Content>
+// <MsgId>1234567890123456</MsgId>
+// <AgentID>128</AgentID>
+// </xml>
 
-为了将此段明文回复给用户，企业应：
-1.自己生成时间时间戳(timestamp),随机数字串(nonce)以便生成消息体签名，也可以直接用从公众平台的post url上解析出的对应值。
-2.将明文加密得到密文。
-3.用密文，步骤1生成的timestamp,nonce和企业在公众平台设定的token生成消息体签名。
-4.将密文，消息体签名，时间戳，随机数字串拼接成xml格式的字符串，发送给企业号。
-以上2，3，4步可以用公众平台提供的库函数EncryptMsg来实现。
-*/
+// 为了将此段明文回复给用户，企业应：
+// 1.自己生成时间时间戳(timestamp),随机数字串(nonce)以便生成消息体签名，也可以直接用从公众平台的post url上解析出的对应值。
+// 2.将明文加密得到密文。
+// 3.用密文，步骤1生成的timestamp,nonce和企业在公众平台设定的token生成消息体签名。
+// 4.将密文，消息体签名，时间戳，随机数字串拼接成xml格式的字符串，发送给企业号。
+// 以上2，3，4步可以用公众平台提供的库函数EncryptMsg来实现。
+// */
 
-// 需要发送的明文
-$sRespData = "<xml><ToUserName><![CDATA[mycreate]]></ToUserName><FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId><AgentID>128</AgentID></xml>";
-$sEncryptMsg = ""; //xml格式的密文
-$errCode = $wxcpt->EncryptMsg($sRespData, $sReqTimeStamp, $sReqNonce, $sEncryptMsg);
-if ($errCode == 0) {
-	// TODO:
-	// 加密成功，企业需要将加密之后的sEncryptMsg返回
-	// HttpUtils.SetResponce($sEncryptMsg);  //回复加密之后的密文
-} else {
-	print("ERR: " . $errCode . "\n\n");
-	// exit(-1);
-}
+// // 需要发送的明文
+// $sRespData = "<xml><ToUserName><![CDATA[mycreate]]></ToUserName><FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId><AgentID>128</AgentID></xml>";
+// $sEncryptMsg = ""; //xml格式的密文
+// $errCode = $wxcpt->EncryptMsg($sRespData, $sReqTimeStamp, $sReqNonce, $sEncryptMsg);
+// if ($errCode == 0) {
+// 	// TODO:
+// 	// 加密成功，企业需要将加密之后的sEncryptMsg返回
+// 	// HttpUtils.SetResponce($sEncryptMsg);  //回复加密之后的密文
+// } else {
+// 	print("ERR: " . $errCode . "\n\n");
+// 	// exit(-1);
+// }
 
